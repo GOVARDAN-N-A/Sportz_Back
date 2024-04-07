73f893d8efb1ebd78075ba5ddb6bd66ef21cc97e
GOVARDAN-N-A <e1215024govardancse@gmail.com>
2024-04-07 19:23:44 +0530

    Removed Depricated

--- index.js
+++ index.js
@@ -151,11 +151,9 @@ app.use(express.json());
 app.use(cors({ origin: ['https://sportz-front.onrender.com/', 'http://10.220.197.84:5173'] }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB Atlas'))
+mongoose.connect(process.env.MONGODB_URI);
+
+then(() => console.log('Connected to MongoDB Atlas'))
 .catch(error => console.error('Error connecting to MongoDB Atlas:', error));
 
 // Socket.io message handling
